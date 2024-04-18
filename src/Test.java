9d230dafe570b16f261011e17f759c9cd0926c56
Nimisha <nimisha07.nimisha@gmail.com>
2024-04-18 13:37:54 -0400

    New method added

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,25 @@
-public class Test
-{
+public class Test {
 
-    public int  add(int x, int y)
-    {
-        return x+y;
+    public int add(int x, int y) {
+        return x + y;
     }
-    public int sub(int x, int y){
-        return x-y;
+
+    public int sub(int x, int y) {
+        return x - y;
     }
-    public int multiply(int x, int y){
-        return  x*y;
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public void divisiblityBy5() {
+        int n = 25;
+        if (n % 5 == 0) {
+            System.out.println("Divisible by 5");
+        } else {
+            System.out.println("Not divisible by 5 ");
+        }
+
+
     }
 }
