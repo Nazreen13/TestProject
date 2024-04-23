168b4f1845cad82356caa1a23c8a0699f7b010e7
Nazreen13 <117366865+Nazreen13@users.noreply.github.com>
2024-04-23 12:17:44 +0530

    Add files via upload

---
+++ DemoFinal/src/Test.java
@@ -0,0 +1,41 @@
+public class Test {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int sub(int x, int y) {
+        return x - y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public void divisiblityBy5() {
+        int n = 25;
+        if (n % 5 == 0) {
+            System.out.println("Divisible by 5");
+        } else {
+            System.out.println("Not divisible by 5 ");
+        }
+
+    }
+
+    public int mod(int a, int b)
+    {
+    	return a%b;}
+
+    public int div(int x, int y) {
+    	return x/y;
+    }
+    public int sumplustwo(int x,int y) {
+    	return x+y+2;
+    	
+    }
+  
+    public double percentage(int x, int y){
+        return (x/y)*100;
+
+    }
+}
