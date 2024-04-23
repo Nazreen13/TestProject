97338c97c11cccb09e10a17545f5c2b78a4e3d4b
Nazreen13 <117366865+Nazreen13@users.noreply.github.com>
2024-04-23 12:19:05 +0530

    Update Main.java

--- DemoFinal/src/Main.java
+++ DemoFinal/src/Main.java
@@ -2,10 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello");
-<<<<<<< HEAD
         System.out.println(mod(15,6));
         
-=======
+
         Test t=new Test();
       int addition=t.add(10,20);
       System.out.println(addition);
@@ -15,7 +14,7 @@ public class Main {
         System.out.println( mul);
        int division= t.div(50,5);
        System.out.println(division);
->>>>>>> ec58feb782ec46bf1c5d3a0024c5bb19ae954400
+
 
 
     }
